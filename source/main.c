08553c12ef1715c805e2fc2677708d6784431720
John Jackson <mrfrenik@gmail.com>
2021-01-26 11:30:48 -0600

    updated main, updated gs

--- source/main.c
+++ source/main.c
@@ -19,12 +19,14 @@ void update()
 		gs_engine_quit();
 	}
 
+	gs_vec2 ws = gs_platform_window_sizev(gs_platform_main_window());
+
 	const float t = gs_platform_elapsed_time() * 0.0001f;
 	gsi_camera3D(&gsi);
 	gsi_rotatefv(&gsi, gs_deg2rad(90.f), GS_ZAXIS); gsi_rotatefv(&gsi, t, GS_YAXIS);
 	gsi_sphere(&gsi, 0.f, 0.f, 0.f, 1.f, 50, 150, 200, 50, GS_GRAPHICS_PRIMITIVE_LINES);
 	gsi_camera2D(&gsi);
-	gsi_text(&gsi, 325.f, 280.f, "Hello, Gunslinger.", NULL, false, 255, 255, 255, 255);
+	gsi_text(&gsi, ws.x * 0.5f - 70.f, ws.y * 0.5f, "Hello, Gunslinger.", NULL, false, 255, 255, 255, 255);
 	gsi_render_pass_submit(&gsi, &cb, gs_color(10, 10, 10, 255));
 
 	// Submit command buffer for GPU
